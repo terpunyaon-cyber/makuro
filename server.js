4b13b0cb51b10f75b1d3c5de1b84a6b064040e07
terpunyaon-cyber <terpunyaon@gmail.com>
2026-06-10 09:11:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const crypto = require("crypto");
 const multer = require("multer");
 
 const app = express();
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
 const PORT = process.env.PORT || 3000;
 const API_SECRET = process.env.API_SECRET || "makuro-secret-123";
